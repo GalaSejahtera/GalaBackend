b5374b442d2e6d9bbc0db18ca8fbcac2ec5e9182
Chin Jia Xiong <chinxiongwei@yahoo.com.my>
2021-01-21 15:52:55 +0800

    Commented crawler

--- pkg/model/covid.go
+++ pkg/model/covid.go
@@ -3,7 +3,6 @@ package model
 import (
 	"context"
 	"galasejahtera/pkg/dto"
-	"galasejahtera/pkg/utility"
 )
 
 // GetCovid gets covid by ID
@@ -15,17 +14,17 @@ func (m *Model) GetCovid(ctx context.Context, id string) (*dto.Covid, error) {
 // QueryCovids queries covids by sort, range, filter
 func (m *Model) QueryCovids(ctx context.Context, sort *dto.SortData, itemsRange *dto.RangeData, filter *dto.FilterData) (int64, []*dto.Covid, error) {
 
-	// TODO: Comment the block that crawls everything and updates database
-	for i := 0; i < 10; i++ {
-		covids := utility.CrawlNews(int64(i))
-		for _, covid := range covids {
-			_, err := m.covidDAO.Get(ctx, covid.ID)
-			if err != nil {
-				// only add into db if not found
-				_, err = m.covidDAO.Create(ctx, covid)
-			}
-		}
-	}
+	// Commented the block that crawls everything and updates database
+	//for i := 0; i < 10; i++ {
+	//	covids := utility.CrawlNews(int64(i))
+	//	for _, covid := range covids {
+	//		_, err := m.covidDAO.Get(ctx, covid.ID)
+	//		if err != nil {
+	//			// only add into db if not found
+	//			_, err = m.covidDAO.Create(ctx, covid)
+	//		}
+	//	}
+	//}
 
 	return m.covidDAO.Query(ctx, sort, itemsRange, filter)
 }
