6f7ccd16b976ec725597960f928fba39d86e8144
Chin Jia Xiong <chinxiongwei@yahoo.com.my>
2021-01-24 16:31:55 +0800

    Path update

--- pkg/api/galasejahtera-service.pb.gw.go
+++ pkg/api/galasejahtera-service.pb.gw.go
@@ -940,30 +940,12 @@ func local_request_GalaSejahteraService_DeleteUsers_0(ctx context.Context, marsh
 }
 
 var (
-	filter_GalaSejahteraService_GetPasswordReset_0 = &utilities.DoubleArray{Encoding: map[string]int{"id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+	filter_GalaSejahteraService_GetPasswordReset_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
 
 func request_GalaSejahteraService_GetPasswordReset_0(ctx context.Context, marshaler runtime.Marshaler, client GalaSejahteraServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetPasswordResetRequest
 	var metadata runtime.ServerMetadata
-
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["id"]
-	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
-	}
-
-	protoReq.Id, err = runtime.String(val)
-
-	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
-	}
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
@@ -980,24 +962,6 @@ func request_GalaSejahteraService_GetPasswordReset_0(ctx context.Context, marsha
 func local_request_GalaSejahteraService_GetPasswordReset_0(ctx context.Context, marshaler runtime.Marshaler, server GalaSejahteraServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetPasswordResetRequest
 	var metadata runtime.ServerMetadata
-
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["id"]
-	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
-	}
-
-	protoReq.Id, err = runtime.String(val)
-
-	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
-	}
 
 	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_GalaSejahteraService_GetPasswordReset_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
@@ -2254,9 +2218,9 @@ var (
 
 	pattern_GalaSejahteraService_DeleteUsers_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "users"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_GalaSejahteraService_GetPasswordReset_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"v1", "users", "id", "passwordreset"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_GalaSejahteraService_GetPasswordReset_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "passwordreset"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_GalaSejahteraService_UpdatePassword_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"v1", "users", "passwordreset", "userId"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_GalaSejahteraService_UpdatePassword_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "passwordreset", "userId"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_GalaSejahteraService_Login_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "login"}, "", runtime.AssumeColonVerbOpt(true)))
 
